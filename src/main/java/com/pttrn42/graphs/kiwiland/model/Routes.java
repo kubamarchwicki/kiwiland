bd43edd0bb7c29fce48421898661b2be6582b421
kubamarchwicki <kuba@marchwicki.pl>
2020-09-04 22:19:04 +0200

    Native distance calculation

--- src/main/java/com/pttrn42/graphs/kiwiland/model/Routes.java
+++ src/main/java/com/pttrn42/graphs/kiwiland/model/Routes.java
@@ -6,17 +6,37 @@ import java.util.List;
 public class Routes {
     private final List<Route> routes = new ArrayList<>();
 
-    public String distance(Town from, Town to) {
+    public String distance(Town... towns) {
+        //TODO: maybe try monad?
+        //TODO: more functional approach
+        try {
+            Integer sumDistance = 0;
+            for (int i = 0; i<towns.length-1; i++) {
+                sumDistance += distance(towns[i], towns[i+1]);
+            }
+            return sumDistance.toString();
+        } catch (NoSuchRouteException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private Integer distance(Town from, Town to) {
         return routes.stream()
                 .filter(route -> route.from().equals(from) && route.to().equals(to))
                 .findFirst()
                 .map(Route::distance)
-                .map(String::valueOf)
-                .orElse("NO SUCH ROUTE");
+                .orElseThrow(() -> new NoSuchRouteException());
     }
 
     public Routes addRoute(Route route) {
         routes.add(route);
         return this;
+    }
+
+    static class NoSuchRouteException extends RuntimeException {
+
+        public NoSuchRouteException() {
+            super("NO SUCH ROUTE");
+        }
     }
 }
--- src/test/java/com/pttrn42/graphs/kiwiland/model/RoutesTest.java
+++ src/test/java/com/pttrn42/graphs/kiwiland/model/RoutesTest.java
@@ -14,6 +14,16 @@ class RoutesTest {
     }
 
     @Test
+    void singleRoute_queryNonExistingRoute_returnsNoSuchRoute() {
+        Town a=new Town("A"), b=new Town("B");
+        Routes routes = new Routes()
+                .addRoute(new Route(a, b, 5));
+
+        var distance = routes.distance(a, new Town("C"));
+        Assertions.assertEquals("NO SUCH ROUTE", distance);
+    }
+
+    @Test
     void singleRoute_returnsRouteDistance() {
         Town a=new Town("A"), b=new Town("B");
         Routes routes = new Routes()
@@ -23,4 +33,27 @@ class RoutesTest {
         Assertions.assertEquals("5", distance);
     }
 
+    @Test
+    void multipleRoutes_returnDistance() {
+        Town a=new Town("A"), b=new Town("B"), c=new Town("C");
+
+        Routes routes = new Routes()
+                .addRoute(new Route(a, b, 5))
+                .addRoute(new Route(b, c, 4));
+
+        var distance = routes.distance(a, b, c);
+        Assertions.assertEquals("9", distance);
+    }
+
+    @Test
+    void multipleRoutes_routesNotExists_returnsNoSuchRoute() {
+        Town a=new Town("A"), e=new Town("E"), d=new Town("D");
+
+        Routes routes = new Routes()
+                .addRoute(new Route(a, e, 7))
+                .addRoute(new Route(d, e, 6));
+
+        var distance = routes.distance(a, e, d);
+        Assertions.assertEquals("NO SUCH ROUTE", distance);
+    }
 }
